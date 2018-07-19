8fbb179c27c463b39bc34066e0fa1d9ae7d571b8
Phil Mareu <philmareu@gmail.com>
2018-07-19 18:24:01 -0500

    fixed spacing issue

--- resources/views/docs/navigation.blade.php
+++ resources/views/docs/navigation.blade.php
@@ -1,19 +1,17 @@
-<div class="uk-width-1-2@s uk-width-2-5@m">
-    <ul class="uk-nav uk-nav-default">
-        <li class="uk-nav-header">Getting Started</li>
-        <li><a href="#">Welcome</a></li>
-        <li><a href="#">Installation</a></li>
-        <li class="uk-nav-divider"></li>
-        <li class="uk-nav-header">Admin Panel</li>
-        <li><a href="">Overview</a></li>
-        <li><a href="#">Resources</a></li>
-        <li><a href="#">Fields</a></li>
-        <li><a href="#">Images</a></li>
-        <li><a href="#">Objects</a></li>
-        <li><a href="#">Feeds</a></li>
-        <li><a href="#">Redirects</a></li>
-        <li class="uk-nav-divider"></li>
-        <li class="uk-nav-header">Supported Extensions</li>
-        <li><a href="#">Laramanager Blog</a></li>
-    </ul>
-</div>
+<ul class="uk-nav uk-nav-default">
+    <li class="uk-nav-header">Getting Started</li>
+    <li><a href="{{ url('/') }}">Welcome</a></li>
+    <li><a href="{{ url('docs/installation') }}">Installation</a></li>
+    <li class="uk-nav-divider"></li>
+    <li class="uk-nav-header">Admin Panel</li>
+    <li><a href="">Overview</a></li>
+    <li><a href="#">Resources</a></li>
+    <li><a href="#">Fields</a></li>
+    <li><a href="#">Images</a></li>
+    <li><a href="#">Objects</a></li>
+    <li><a href="#">Feeds</a></li>
+    <li><a href="#">Redirects</a></li>
+    <li class="uk-nav-divider"></li>
+    <li class="uk-nav-header">Supported Extensions</li>
+    <li><a href="#">Laramanager Blog</a></li>
+</ul>
