9da987e622fef64f07032923d5660eaba48162e0
Phil Mareu <philmareu@gmail.com>
2018-07-20 16:36:11 -0500

    basic homepage layout

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -9,5 +9,25 @@
 @endsection
 
 @section('default-content')
-
+    <header class="uk-text-center uk-margin-large">
+        <h1>Laramanager</h1>
+        <p>Laramanager is a basic admin tool for Laravel applications created by <a href="https://philmareu.com">Phil Mareu.</a></p>
+        <a href="https://github.com/philmareu/laramanager" class="uk-button uk-button-default"><span uk-icon="icon: github" class="uk-margin-small-right"></span>Github</a>
+    </header>
+    <div class="uk-container uk-margin">
+        <div class="uk-child-width-1-2" uk-grid>
+            <div>
+                <img src="{{ url('images/original/laramanager-install.jpg') }}" alt="">
+            </div>
+            <div>
+                <img src="{{ url('images/original/laramanager-resources-create.jpg') }}" alt="">
+            </div>
+            <div>
+                <img src="{{ url('images/original/laramanager-images-populated.jpg') }}" alt="">
+            </div>
+            <div>
+                <img src="{{ url('images/original/laramanager-images-edit-image.jpg') }}" alt="">
+            </div>
+        </div>
+    </div>
 @endsection
--- resources/views/layout/navigation/primary.blade.php
+++ resources/views/layout/navigation/primary.blade.php
@@ -1,6 +1,6 @@
 <nav class="uk-navbar-container" uk-navbar>
     <div class="uk-navbar-left">
-        <a class="uk-navbar-item uk-logo" href="#">Laramanager</a>
+        <a class="uk-navbar-item uk-logo" href="{{ url('/') }}">Laramanager</a>
 
         <ul class="uk-navbar-nav">
             <li class="uk-active"><a href="{{ url('/docs') }}">Docs</a></li>
